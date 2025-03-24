369af99bc6c2c94e71b4434e0a24dad9be7f8b2f
alwaysssraeee26 <raechatt06@gmail.com>
2025-03-24 16:53:50 -0400

    Submission hw9-19.cpp

--- hw9-19.cpp
+++ hw9-19.cpp
@@ -1,4 +1,4 @@
-// Lab9-2.cpp - displays two monthly car payments
+// Lab9-19.cpp - displays two monthly car payments and total amounts
 // Created/revised by <your name> on <current date>
 
 #include <iostream>
@@ -7,7 +7,7 @@
 using namespace std;
 
 // Function prototype
-void getPayment(int, double, int, double&, double&);
+double getPayment(int, double, int);
 
 int main()
 {
@@ -33,42 +33,44 @@ int main()
     cout << "Term in years: ";
     cin >> term;
 
-    // Calculate payments
-    getPayment(carPrice - rebate, creditRate / 12, term * 12, creditPayment, creditTotal);
-    getPayment(carPrice, dealerRate / 12, term * 12, dealerPayment, dealerTotal);
+    // Call function to calculate payments
+    creditPayment = getPayment(carPrice - rebate, creditRate / 12, term * 12);
+    dealerPayment = getPayment(carPrice, dealerRate / 12, term * 12);
 
-    // Display results
+    // Calculate total amounts
+    if (creditPayment != -1)
+        creditTotal = creditPayment * (term * 12);
+    if (dealerPayment != -1)
+        dealerTotal = dealerPayment * (term * 12);
+
+    // Display payments and totals
     cout << fixed << setprecision(2) << endl;
+
     if (creditPayment != -1)
     {
-        cout << "Credit union payment: $" << creditPayment << endl;
+        cout << "Credit union monthly payment: $" << creditPayment << endl;
         cout << "Total amount paid through credit union: $" << creditTotal << endl;
     }
     else
-        cout << "Credit union payment calculation error (invalid denominator)." << endl;
+        cout << "Error: Invalid calculation for credit union payment." << endl;
 
     if (dealerPayment != -1)
     {
-        cout << "Dealer payment: $" << dealerPayment << endl;
+        cout << "Dealer monthly payment: $" << dealerPayment << endl;
         cout << "Total amount paid through dealer: $" << dealerTotal << endl;
     }
     else
-        cout << "Dealer payment calculation error (invalid denominator)." << endl;
+        cout << "Error: Invalid calculation for dealer payment." << endl;
 
     return 0;
 }
 
-// Function definition
-void getPayment(int prin, double monthRate, int months, double& monthlyPayment, double& totalPayment)
+// ****** Function Definition ******
+double getPayment(int prin, double monthRate, int months)
 {
     if (monthRate == 0 || months <= 0)
-    {
-        monthlyPayment = -1; // Return -1 if the denominator is invalid
-        totalPayment = -1;
-    }
-    else
-    {
-        monthlyPayment = prin * monthRate / (1 - pow(1 + monthRate, -months));
-        totalPayment = monthlyPayment * months;
-    }
+        return -1; // Return -1 if the denominator is invalid
+
+    double monthPay = prin * monthRate / (1 - pow(1 + monthRate, -months));
+    return monthPay;
 }
